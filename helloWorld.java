78481cf5b5955e3c3ae745d99ea73909ae5dcc14
Karla Shanessa <2381044@unai.edu>
2024-10-10 08:57:27 +0700

    trial

--- helloWorld.java
+++ helloWorld.java
@@ -2,5 +2,6 @@ public class helloWorld {
     public static void main(String[] args) {
         System.out.println("haloooou");
         System.out.println("ini Helena yg nambahin");
+        System.out.println("Karla Shanessa Tauran");
     }
 }
