5486e8a5fed36e56e42102e728e8362019c9c794
Helena Onezia Lewi <2381003@unai.edu>
2024-10-11 07:01:27 +0700

    trialhol

--- project/Main.java
+++ project/Main.java
@@ -10,21 +10,27 @@ public class Main {
         System.out.println("Masukkan pilihan Anda: ");
         int choice = input.nextInt();
 
+        switch(choice){
+            case 1:
+                System.out.println("proses");;
+            break;
+
+            case 2:
+                System.out.println("proses");;
+            break;
+
+            default:
+                System.out.println("Pilih dari pilihan 1-x");
+        }
 
     }
 
     public static void pilihanMenu(){
         System.out.println("========== Sistem Manajemen Event ==========");
-        System.out.println("1. Tambah Event");
-        System.out.println("2. Hapus Event");
-        System.out.println("3. Edit Detail Event");
-        System.out.println("4. Lihat Semua Event");
-        System.out.println("5. Cari Event");
-        System.out.println("6. Daftar Peserta Event");
-        System.out.println("7. Tambah Peserta Event");
-        System.out.println("8. Hapus Peserta Event");
-        System.out.println("9. Lihat Total Peserta");
-        System.out.println("10. Lihat Detail Peserta");
+        System.out.println("1. Event");
+        System.out.println("2. Peserta Event");
+        System.out.println("3. Lihat Total Peserta");
+        System.out.println("4. Lihat Detail Peserta");
         System.out.println("x. Keluar ");
     }
 }
