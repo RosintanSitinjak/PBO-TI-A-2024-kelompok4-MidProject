ac5db1e45108102a87606865438f32b99606a6c9
Rosintan Sitinjak <2381014@unai.edu>
2024-10-14 11:14:50 +0700

    memperbaharui/memindahkan/menyesuaikan codingan di class Event dengan EventManager

--- project/EventManager.java
+++ project/EventManager.java
@@ -1,83 +1,87 @@
 package project;
 
 import java.util.ArrayList;
+
 import java.util.Scanner;
 
 public class EventManager {
-        static ArrayList<Event> events = new ArrayList<>();
-        static Scanner input = new Scanner(System.in);
+    static ArrayList<Event> events = new ArrayList<>();
+    static Scanner input = new Scanner(System.in);
 
-        public static void addEvent(){
-            System.out.println("Nama Event: ");
-            String namaEvent = input.nextLine();
-            System.out.println("Tanggal Event: ");
-            String tanggalEvent = input.nextLine();
-            System.out.println("Tempat Event: ");
-            String tempatEvent = input.nextLine();
+    public static void addEvent(){
+        System.out.print("Nama Event: ");
+        String nameEvent = input.nextLine();
 
-            events.add(new Event(namaEvent, tanggalEvent, tempatEvent));
-            System.out.println("\nEvent berhasil ditambahkan!");
-            System.out.println("Kembali ke menu? (y/n): ");
-            String back = input.nextLine();
+        System.out.print("Tanggal Event: ");
+        String dateEvent = input.nextLine();
 
-            if(back.equals("y")){
-                menuEvent();
-            } else {
-                return;
-            }
+        System.out.print("Tempat/Lokasi Event: ");
+        String eventLocation = input.nextLine();
+
+        events.add(new Event(nameEvent, dateEvent, eventLocation));
+        System.out.println("Event berhasil ditambahkan");
+        System.out.print("Kembali ke menu awal? (y/n): ");
+        String back = input.nextLine();
+
+        if (back.equals("y")){
+            menuEvent();
+        } else {
+            return;
+        }
+    }
+
+    public static void showEvent(){
+        if (events.isEmpty()){
+            System.out.println("Tidak ada event yang terdaftar");
+            return;
         }
 
-        public static void showEvent(){
-            if (events.isEmpty()){
-                System.out.println("Tidak ada event yang tersedia.");
-                return;
-            }
+        for (int i = 0; i < events.size(); i++){
+            Event event = events.get(i);
 
-            for (int i = 0; i < events.size(); i++){
-                Event event = events.get(i);
-                System.out.println((i+1) + ". Nama Event: " + event.namaEvent);
-                System.out.println("Tanggal Event: " + event.tanggalEvent);
-                System.out.println("Tempat Event : " + event.tempatEvent);
-                System.out.println(" ");
-            }
-            System.out.println("Kembali ke menu? (y/n): ");
-            String back = input.nextLine();
-
-            if(back.equals("y")){
-                menuEvent();
-            } else {
-                return;
-            }
+            System.out.println((i + 1) + ". Nama Event: " + event.namaEvent);
+            System.out.println("Tanggal Event: " + event.tanggalEvent);
+            System.out.println("Tempat Event: " + event.tempatEvent);
+            System.out.println(" ");
         }
 
-        public static void menuEvent(){
-            System.out.println("== MENU EVENT ==");
-            System.out.println("1. Tambah Event");
-            System.out.println("2. Hapus Event");
-            System.out.println("3. Edit Event");
-            System.out.println("4. Lihat Event");
-            System.out.println("0. Keluar");
-            System.out.println("\nPilih Menu: ");
-            int pilih = input.nextInt();
+        System.out.print("Kembali ke menu? (y/n): ");
+        String back = input.nextLine();
+
+        if (back.equals("y")){
+            menuEvent();
+        } else {
+            return;
+        }
+    }
+
+    public static void menuEvent(){
+        System.out.println("=== Menu Event ===");
+        System.out.println("1. Tambah Event");
+        System.out.println("2. Hapus Event");
+        System.out.println("3. Edit Event");
+        System.out.println("4. Lihat Event");
+        System.out.println("0. Kembali ke Menu Utama");
+        System.out.print("Masukkan Pilihan Menu: ");
+            int choose = input.nextInt();
             input.nextLine();
 
-            switch(pilih){
+            switch (choose){
                 case 1:
                     addEvent();
                     break;
                 case 2:
-
                     break;
                 case 3:
-
                     break;
                 case 4:
                     showEvent();
-                     break;
+                    break;
                 case 0:
-                    return;
+                    break;
                 default:
-                    System.out.println("Pilihan menu tidak valid.");
+                    System.out.println("Pilihan menu tidak valid, silahkan masukkan pilihan menu kembali");
+
             }
-        }
     }
+}
